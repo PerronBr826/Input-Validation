c0b97c3f3ac52a7ba4c4801d526ec57184b76908
PerronBr826 <PerronBr826@northwested.org>
2024-12-10 10:04:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ string = input("Enter a string: ")
 string_len = len(string)
 
 
-if string.isalpha() or string.isdigit() and len > 5:
+if string.isalpha() or string.isdigit() and string_len > 5 and string_len < 11:
+    print(f'Valid Input: {string}')
