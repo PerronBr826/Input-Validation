966dd0b4f2a1ec18d88027db535667be90edee36
PerronBr826 <PerronBr826@northwested.org>
2024-12-10 10:07:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ string = input("Enter a string: ")
 string_len = len(string)
 
 
-if string.isalpha() or string.isdigit() and string_len > 5 and string_len < 11:
-    print(f'Valid Input: {string}')
+if string.isalpha() or string.isdigit():
+    if string_len > 5 and string_len < 11:
+        print(f'Valid Input: {string}')
+    else:
+        print("Input must be between 6 and 10 characters long.")
+else:
+    print("Input must be alphabetic or numeric (but not both).")
